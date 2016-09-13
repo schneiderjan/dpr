01ceac8df5bbcd78d4a13d4aa922a48560da3167
Jan-Niklas Schneider <heisse.cola@gmail.com>
2016-09-13 11:33:23 +0200

    Added unit test project + two test class: storm,weather

---
+++ DprObserverPattern/DprObserverPatternTest/WeatherTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DprObserverPatternTest
+{
+    [TestClass]
+    public class WeatherTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+
+        }
+    }
+}
