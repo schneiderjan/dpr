5b9431286d627f7cbdf3594d2a10c4be7464487c
Jan-Niklas Schneider <heisse.cola@gmail.com>
2016-10-04 14:26:39 +0200

    Added Channel.cs

---
+++ DprIteratorPattern/DprIteratorPattern/Channel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DprIteratorPattern
+{
+    public class Channel : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name { get { return name; } set { name = value; NotifyPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
