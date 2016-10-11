71bcd0fd614d299ec930f566a0e3e2dbc6d7849c
Jan-Niklas Schneider <heisse.cola@gmail.com>
2016-10-11 20:46:06 +0200

    Fixed unit test

---
+++ DprStatePattern/DprStatePatternTest/StateComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using DprStatePattern;
+
+namespace DprStatePatternTest
+{
+    /// <summary>
+    /// CollectionAssert.AreEqual doesnt properly see if values are equal
+    /// Hence this class is needed.
+    /// reference: http://softwareonastring.com/357/why-collectionassert-areequal-fails-even-when-both-lists-contain-the-same-items
+    /// </summary>
+    public class StateComparer : Comparer<State>
+    {
+        public override int Compare(State x, State y) => x.Action.CompareTo(y.Action);
+    }
+
+}
