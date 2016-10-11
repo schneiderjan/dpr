993f3fe708c50cac81a7620a1a4da38cf78553a8
Jan-Niklas Schneider <heisse.cola@gmail.com>
2016-10-11 20:38:08 +0200

    Added unit test

---
+++ DprStatePattern/DprStatePatternTest/StateTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DprStatePattern;
+using System.Collections.Generic;
+
+namespace DprStatePatternTest
+{
+    [TestClass]
+    public class StateTest
+    {
+        private static ProductionChain prodChain;
+
+        [TestMethod]
+        public void StateValidOrder_Test()
+        {
+            //Arrange
+            prodChain = new ProductionChain();
+            List<State> actualStates = new List<State>();
+            List<State> expectedStates = new List<State>
+            {
+                new Initialized(),
+                new PreManufacturing(),
+                new Manufacturing(),
+                new PostManufacturing(),
+                new Terminated()
+            };
+
+            //Act
+            while (!(prodChain.GetState() is Terminated))
+            {
+                prodChain.Pull();
+                actualStates.Add(prodChain.GetState());
+            }
+
+            //Assert
+            CollectionAssert.AreEqual(expectedStates, actualStates);
+        }
+    }
+}
